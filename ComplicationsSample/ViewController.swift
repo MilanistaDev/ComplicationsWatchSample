f78a84cb7c5bd994ab71e3fdd372102e7c7d71dd
Takuya Aso <milanista224@gmail.com>
2018-11-19 11:30:47 +0900

    Initial Commit

---
+++ ComplicationsSample/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  ComplicationsSample
+//
+//  Created by 麻生 拓弥 on 2018/11/19.
+//  Copyright © 2018年 com.ASTK. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
